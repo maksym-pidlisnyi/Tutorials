74433d0267934f68672c2dcba1720301c7ed9f21
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2020-08-07 00:01:14 +0300

    add button initial functionality

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.tutorials
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val btnApply : Button = findViewById(R.id.btnApply)
+        btnApply.setOnClickListener {
+            val firsName = editTextFirstName.text.toString()
+            val lastName = editTextLastName.text.toString()
+            val birthDate = editTextBirthDate.text.toString()
+            val country = editTextCountry.text.toString()
+            Log.d("MainActivity", "$firsName $lastName, born on $birthDate, in $country")
+
+        }
     }
 }
